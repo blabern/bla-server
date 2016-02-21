c99d069b644932c07b6969678f350b74dba8d6a2
Oleg Slobodskoi <oleg008@gmail.com>
2016-02-21 17:53:36 +0100

    use heroku port

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ var server = require('http').Server(app)
 var io = require('socket.io')(server)
 var translate = require('./translate')
 
-server.listen(3000, () => {
-  console.log('Listening on port 3000!')
+var port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log('Listening on port', port)
 })
 
 var socket
